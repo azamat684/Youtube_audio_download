54ac1b63ae5076e3a84ce5bbe7b8a5f828dafe13
Azamat Dosmukhambetov <122520658+azamat684@users.noreply.github.com>
2023-11-14 23:34:07 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,5 +6,5 @@ from loader import dp
 
 @dp.message_handler(CommandStart(),state="*")
 async def bot_start(message: types.Message,state: FSMContext):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Salom, {message.from_user.full_name}, botga xush kelibsiz!")
     await state.finish()
